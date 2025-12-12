578942c6237f7c740e27c77720563eb258822053
KNUT0308 <erennel@kth.se>
2025-12-12 09:57:17 +0100

    Gjorde en liten ändring

--- Assignment4/question8/question8.c
+++ Assignment4/question8/question8.c
@@ -6,6 +6,48 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+void readAndWrite(pid_t pid, void* mmaped_ptr) {
+    if (pid == 0) {
+        // Child process
+        //
+        // Child message
+        char* message = "01234";
+        // Memory mapping
+        printf("Child process (pid=%d); mmap address %p\n", pid, mmaped_ptr);
+
+        // Memory address for child write
+        int* child_target = (int*)(char*)mmaped_ptr + 0;
+
+        memcpy(child_target, message, strlen(message));
+
+        msync(child_target, strlen(message), MS_SYNC);
+        sleep(1);
+
+        // Memory address for the parent write
+        char* parent_target = (char*)(mmaped_ptr + 4096);
+
+        printf("child process (pid=%d); read from mmaped_ptr[4096] %s\n", pid, parent_target);
+
+    } else {
+        // Parent process
+        //
+        // Parent message
+        char* message = "56789";
+        // Memory mapping
+        printf("Parent process (pid=%d); mmap address %p\n", pid, mmaped_ptr);
+
+        // Write message to parent address
+        int* parent_target = (int*)(char*)(mmaped_ptr + 4096);
+
+        memcpy(parent_target, message, strlen(message));
+        msync(parent_target, strlen(message), MS_SYNC);
+        sleep(1);
+        // Get message from child address
+        char* child_target = (char*)mmaped_ptr + 0;
+        printf("Parent process (pid=%d); read from mmaped_ptr[0] %s\n", pid, child_target);
+    }
+}
+
 void runProcesses() {
     int size = 4096 + 10;
 
@@ -27,46 +69,8 @@ void runProcesses() {
     }
 
     void* mmaped_ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    readAndWrite(pid, mmaped_ptr);
 
-    if (pid == 0) {
-        // Child process
-        //
-        // Child message
-        char* message = "01234";
-        // Memory mapping
-        printf("Child process (pid=%d); mmap address %d\n", pid, mmaped_ptr);
-
-        // Memory address for child write
-        int* child_target = (int*)(char*)mmaped_ptr + 0;
-
-        memcpy(child_target, message, strlen(message));
-
-        msync(child_target, strlen(message), MS_SYNC);
-        sleep(1);
-
-        // Memory address for the parent write
-        char* parent_target = (char*)(mmaped_ptr + 4096);
-
-        printf("child process (pid=%d); read from mmaped_ptr[4096] %s\n", pid, parent_target);
-
-    } else {
-        // Parent process
-        //
-        // Parent message
-        char* message = "56789";
-        // Memory mapping
-        printf("Parent process (pid=%d); mmap address %d\n", pid, mmaped_ptr);
-
-        // Write message to parent address
-        int* parent_target = (int*)(char*)(mmaped_ptr + 4096);
-
-        memcpy(parent_target, message, strlen(message));
-        msync(parent_target, strlen(message), MS_SYNC);
-        sleep(1);
-        // Get message from child address
-        char* child_target = (char*)mmaped_ptr + 0;
-        printf("Parent process (pid=%d); read from mmaped_ptr[0] %s\n", pid, child_target);
-    }
     // Close
     munmap(mmaped_ptr, size);
     close(fd);
